c4b40d10228fa7775e6b87de689aacee42029939
shafikul7 <shafikul1170@gmail.com>
2024-07-05 22:56:24 +0600

    task management test

--- Task-Management-React/src/components/Task/TaskBoard/TaskBoard.jsx
+++ Task-Management-React/src/components/Task/TaskBoard/TaskBoard.jsx
@@ -1,13 +1,12 @@
 import { useState } from "react";
+import AddTaskModal from "../AddATaskModal/AddATaskModal";
 import SearchTask from "../SearchTask/SearchTask";
 import TaskAction from "../TaskAction/TaskAction";
 import TaskList from "../TaskList/TaskList";
 
 const TaskBoard = () => {
-  const [task, setTask] = useState([]);
-
   const defaultTask = {
-    id: Crypto.randomUUID(),
+    id: Crypto.randomUUID,
     title: "Learning React",
     description:
       "I want to learn react such that I can treat it like my slave and make it do whatever i want to do",
@@ -15,16 +14,22 @@ const TaskBoard = () => {
     priority: "High",
     isFavorite: true,
   };
+  const [tasks, setTask] = useState([defaultTask]);
+  const [showAddModel, setShowAddModel] = useState(false);
+  function handleAddTask() {
+    setShowAddModel(true);
+  }
   return (
     <div>
       <section className="mb-20" id="tasks">
+        {showAddModel && <AddTaskModal/>}
         <div className="container">
           <div className="p-2 flex justify-end">
             <SearchTask></SearchTask>
           </div>
           <div className="rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16">
-            <TaskAction></TaskAction>
-            <TaskList></TaskList>
+            <TaskAction onAddClick={handleAddTask}></TaskAction>
+            <TaskList tasks={tasks}></TaskList>
           </div>
         </div>
       </section>
